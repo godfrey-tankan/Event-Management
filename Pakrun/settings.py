2b4caad11545b1a58afaa733e11545501f4c6244
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-04-19 14:31:40 +0200

    Update barcode scanning functionality and Instascan library path

--- Pakrun/settings.py
+++ Pakrun/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Events/static'),
+]
 
-STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
